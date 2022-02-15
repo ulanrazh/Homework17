2c4177343b2c9e236961ee4316e3fe3ba52cd83c
Beksultan Mamatkadyr uulu <90905002+beksultanFromPeaksoft@users.noreply.github.com>
2022-02-15 09:53:00 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+        //good job
+        //create your own fields || methods with your logic
         Animal[] animals = {new Shark(), new Turtle(), new Eagle()};
 
         for (Animal animal: animals) {
@@ -23,6 +25,20 @@ public class Main {
             System.out.println(animal.getClass());
         }
         System.out.println(Arrays.toString(animals));
+        // your algorithm is true, but you have to spend a lot of time to see result!
+        // i mean your code works slowly
+        
+       //my version
+        
+        for (Animal animal : animals) {
+            if (animal instanceof Shark) {
+                animal.move();
+            } else  if (animal instanceof Turtle) {
+                animal.move();
+            } else if (animal instanceof Eagle) {
+                animal.move();
+            }
+        }
 //
 //        Shark[] sharks = {(Shark) animals[0]};
 //        Turtle[] turtles = {(Turtle) animals[1]};
